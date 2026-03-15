d929dab9af0e930c4ec7896f568d2d4a48f43d27
Paola <paolaleonardiruvinski@gmail.com>
2026-03-15 16:03:57 -0300

    Organizando

--- Atividade02.py
+++ Atividade02.py
@@ -1,11 +1,10 @@
-#¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸
 
 #No Exercício 01, o programa pede para o usuário informar o ano de nascimento.
 #Esse valor é digitado pelo usuário e armazenado em uma variável.
 #Como o input sempre recebe o valor como texto, é necessário usar int() para converter para número inteiro. 
 #Depois disso, o programa subtrai o ano de nascimento do ano atual (2026) para calcular a idade.
 
-print ('Exercício 01') # → serve para mostrar algo na tela
+print ('Exercício 01') # serve para mostrar algo na tela, no caso: o n° do exercício.
 
 nascimento = input ('Qual sua data de nascimento?') # o input pede para o usuário digitar algo, esse algo vai se tornar a variável
 print () # apenas para pular uma linha
@@ -18,7 +17,7 @@ idade= int (ano_atual) - int (nascimento)
 # VARIÁVEL = Pode assumir qualquer valor
 # CONSTANTE = Não muda
 
-#¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸
+#
 
 #No Exercício 02, é definido o valor de um carro (100 reais). 
 #Em seguida, o programa pergunta quantos carros o usuário deseja comprar.
@@ -35,10 +34,11 @@ valor_total = (int (quantidade) * int (carro))
 print (f" O valor dos carros fica: {valor_total} reais")
 # o F antes das aspas deixa colocar variáveis dentro do texto
 
-#¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸
+#
 
 #No Exercício 03, o usuário informa uma temperatura em Celsius. 
-#O programa utiliza a fórmula de conversão para Fahrenheit (C × 9/5 + 32) para transformar a temperatura e mostrar o resultado na tela.
+#O programa utiliza a fórmula de conversão para Fahrenheit (C × 9/5 + 32) 
+#para transformar a temperatura e mostrar o resultado na tela.
 
 print ('Exercício 03')
 
@@ -50,9 +50,10 @@ print ( f" temperatura em Fahrenheit: {int(Fahrenheit)}")
 
 # Usar o {int()} faz com que o valor final fique sem o .0
 
-#¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸
+#
 
-#No Exercício 04, o programa pede quatro notas ao usuário. Depois, ele soma todas as notas e divide por quatro para calcular a média final.
+#No Exercício 04, o programa pede quatro notas ao usuário. 
+#Depois, ele soma todas as notas e divide por quatro para calcular a média final.
 
 print ('Exercício 04')
 
@@ -64,9 +65,11 @@ Nota4= input ("Qual sua quarta nota?")
 Soma= (int (Nota1) + int (Nota2) + int (Nota3) + int (Nota4))
 print (f" Sua nota final é {int (Soma) / 4 }")
 
-#¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸
+#
 
-#No Exercício 05, o programa pede o ano de nascimento novamente, calcula a idade usando o ano atual e depois multiplica a idade por 12 para descobrir a idade em meses.
+#No Exercício 05, o programa pede o ano de nascimento novamente,
+#calcula a idade usando o ano atual e depois multiplica a idade por 12 
+#para descobrir a idade em meses.
 
 print ('Exercício 05')
 
@@ -78,5 +81,3 @@ Idade = Ano_Atual - nascimento
 idade_meses= Idade * 12
 
 print (f"Sua idade em meses seria: {idade_meses}")
-
-#¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸¸,ø¤°`°¤ø,¸   
