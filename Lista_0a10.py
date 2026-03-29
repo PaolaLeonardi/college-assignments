a0b9d424bbec8d6c57249560694470c45bfc0c6c
Paola <paolaleonardiruvinski@gmail.com>
2026-03-28 21:15:02 -0300

    Create Lista_0a10.py

---
+++ Lista_0a10.py
@@ -0,0 +1,13 @@
+print ("Exercício 01") # Esse código gera uma lista de 1 a 10
+
+numero = 1  
+while numero <= 10:
+  print (numero)
+  numero = numero +1
+  
+print ("Exercício 02") # Esse código gera uma lista de 10 a 0
+
+numero = 10  
+while numero > 0:
+  print (numero)
+  numero -=1
