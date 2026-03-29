1694de74be59e20dc11ff059457f5593d7d188c2
Paola <paolaleonardiruvinski@gmail.com>
2026-03-28 21:18:33 -0300

    Update Estruturas_de_Repeticao(While).py

--- Estruturas_de_Repeticao(While).py
+++ Estruturas_de_Repeticao(While).py
@@ -6,7 +6,7 @@ while numero > 10 or numero < 0: # Ou posso usar 0 < numero < 10 se preferir (no
     print ("Inválido, digite uma número de 0 a 10")
     numero = int(input("Digite um número de 0 a 10: ")) # Repete
 
-print (f"sua nota é {numero}") # Dentro de {} você coloca a variável direto
+print (f"sua nota é {numero}") # Dentro de {} você coloca a variável
 
 #Proximos ex. a fazer: ----------------------------------------------------
 
